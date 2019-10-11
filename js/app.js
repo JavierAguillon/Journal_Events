4aab58482ec4bd3a2bc07a10f1f8925c1f9ef5ce
Javier Aguillon <00078813@uca.edu.sv>
2019-10-11 08:25:11 -0600

    Añadiendo Variables

---
+++ js/app.js
@@ -0,0 +1,14 @@
+//Variables xD
+let date_field = document.querySelector('#date_field')
+let event_field = document.querySelector('#event_field')
+let accident_check = document.querySelector('#accident_check')
+let submit_btn = document.querySelector('#submit_btn')
+let table_body = document.querySelector('#table_body')
+
+//
+let addEvent = (date, event, accident) => {
+    let new_row = document.createElement('tr')
+    
+    
+}
+
