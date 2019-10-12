e9e55b5aa022882afdcec24ab574118a97e1d2ae
David Massana <00072416@uca.edu.sv>
2019-10-12 17:18:59 -0600

    Finished

--- js/app.js
+++ js/app.js
@@ -3,12 +3,33 @@ let date_field = document.querySelector('#date_field')
 let event_field = document.querySelector('#event_field')
 let accident_check = document.querySelector('#accident_check')
 let submit_btn = document.querySelector('#submit_btn')
+
 let table_body = document.querySelector('#table_body')
 
-//
+
 let addEvent = (date, event, accident) => {
     let new_row = document.createElement('tr')
     
-    
+    new_row.className = 'table_active'
+    new_row.innerHTML = `<th scope = 'row'> ${date} </th>
+    <td> ${event} </td>
+    <td> ${accident} </td>`
+    table_body.appendChild(new_row)
 }
+
+let parseAccidentCheck = (value) => {
+    if (value) {
+        return 'YES'
+    }
+    return 'THANKS TO GOD NO.'
+}
+
+
+submit_btn.addEventListener('click', function(){
+    let date = date_field.value
+    let event = event_field.value
+    let accident = parseAccidentCheck(accident_check.checked)
+
+    addEvent(date, event, accident)
+})
 
